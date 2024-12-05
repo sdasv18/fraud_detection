28219f8a19b8e9763d75faee8142d13d1116672b
sdasv18 <sdas.v18@gmail.com>
2024-12-05 11:01:45 +0530

    added 3 new ml model code

--- ml_model.py
+++ ml_model.py
@@ -1,3 +1,14 @@
 
 print("adding all ML related code here")
 
+
+def model1():
+    print("adding code Model1")
+
+
+def model2():
+    print("adding code Model2")
+
+
+def model2():
+    print("adding code Model2")
